47774b27d50b6ac610557f2593acfa24d562b6c3
Jiří Janda <jiri.janda@elinet.cz>
2026-02-06 15:37:07 +0100

    Improve cookies page layout

--- src/pages/CookiePolicyPage.js
+++ src/pages/CookiePolicyPage.js
@@ -1,4 +1,14 @@
-import { MDBBtn, MDBContainer, MDBListGroup, MDBListGroupItem, MDBTypography } from "mdb-react-ui-kit";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 import { clearCookieConsent, getCookieConsent } from "../components/CookieConsent";
 
 const CookiePolicyPage = () => {
@@ -8,64 +18,74 @@ const CookiePolicyPage = () => {
         {"Cookies"}
       </MDBTypography>
 
-      <MDBTypography className="text-start mb-3">
+      <MDBTypography className="text-start mb-4">
         {
-          "Tento web používá nezbytné cookies potřebné pro jeho funkčnost (např. přihlášení a udržení relace)."
+          "Tento web používá nezbytné cookies potřebné pro jeho funkčnost (např. přihlášení a udržení relace). Volitelné cookies (např. analytické nebo marketingové) aktuálně nepoužíváme. Pokud je v budoucnu přidáme, budete je moci povolit/odmítnout v cookie liště."
         }
       </MDBTypography>
 
-      <MDBTypography className="text-start mb-3">
-        {
-          "Volitelné cookies (např. analytické nebo marketingové) aktuálně nepoužíváme. Pokud je v budoucnu přidáme, budete je moci povolit/odmítnout v cookie liště."
-        }
-      </MDBTypography>
+      <MDBRow className="g-4">
+        <MDBCol lg="8">
+          <MDBCard className="h-100">
+            <MDBCardBody>
+              <MDBTypography tag="h5" className="mb-2 text-start">
+                {"Nezbytné cookies používané tímto webem"}
+              </MDBTypography>
+              <MDBTypography className="text-start mb-3">
+                {"Tyto cookies jsou potřeba pro přihlášení a práci s účtem:"}
+              </MDBTypography>
 
-      <MDBTypography tag="h5" className="mb-2 text-start">
-        {"Nezbytné cookies používané tímto webem"}
-      </MDBTypography>
-      <MDBTypography className="text-start mb-2">
-        {"Tyto cookies jsou potřeba pro přihlášení a práci s účtem:"}
-      </MDBTypography>
-      <MDBListGroup className="mb-4">
-        <MDBListGroupItem>
-          <b>{"token"}</b>
-          {" – přihlašovací token (udržení relace po přihlášení)."}
-        </MDBListGroupItem>
-        <MDBListGroupItem>
-          <b>{"email"}</b>
-          {" – e‑mail přihlášeného uživatele (pro identifikaci v aplikaci)."}
-        </MDBListGroupItem>
-        <MDBListGroupItem>
-          <b>{"role"}</b>
-          {" – role uživatele (např. uživatel/admin)."}
-        </MDBListGroupItem>
-        <MDBListGroupItem>
-          <b>{"userName"}</b>
-          {" – jméno uživatele pro zobrazení v menu."}
-        </MDBListGroupItem>
-        <MDBListGroupItem>
-          <b>{"rights"}</b>
-          {" – práva uživatele (řízení přístupů k funkcím)."}
-        </MDBListGroupItem>
-      </MDBListGroup>
+              <MDBListGroup>
+                <MDBListGroupItem>
+                  <b>{"token"}</b>
+                  {" – přihlašovací token (udržení relace po přihlášení)."}
+                </MDBListGroupItem>
+                <MDBListGroupItem>
+                  <b>{"email"}</b>
+                  {" – e‑mail přihlášeného uživatele (pro identifikaci v aplikaci)."}
+                </MDBListGroupItem>
+                <MDBListGroupItem>
+                  <b>{"role"}</b>
+                  {" – role uživatele (např. uživatel/admin)."}
+                </MDBListGroupItem>
+                <MDBListGroupItem>
+                  <b>{"userName"}</b>
+                  {" – jméno uživatele pro zobrazení v menu."}
+                </MDBListGroupItem>
+                <MDBListGroupItem>
+                  <b>{"rights"}</b>
+                  {" – práva uživatele (řízení přístupů k funkcím)."}
+                </MDBListGroupItem>
+              </MDBListGroup>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
 
-      <MDBTypography tag="h5" className="mb-2 text-start">
-        {"Nastavení souhlasu"}
-      </MDBTypography>
-      <MDBTypography className="text-start mb-3">
-        {"Aktuální volba: "}
-        <b>{getCookieConsent() || "nezvoleno"}</b>
-        {"."}
-      </MDBTypography>
-      <div className="d-flex flex-wrap gap-2 mb-4">
-        <MDBBtn
-          color="secondary"
-          type="button"
-          onClick={() => clearCookieConsent()}
-        >
-          {"Změnit volbu cookies"}
-        </MDBBtn>
-      </div>
+        <MDBCol lg="4">
+          <MDBCard className="h-100">
+            <MDBCardBody className="d-flex flex-column">
+              <MDBTypography tag="h5" className="mb-2 text-start">
+                {"Nastavení souhlasu"}
+              </MDBTypography>
+              <MDBTypography className="text-start mb-3">
+                {"Aktuální volba: "}
+                <b>{getCookieConsent() || "nezvoleno"}</b>
+                {"."}
+              </MDBTypography>
+
+              <div className="mt-auto d-flex flex-wrap gap-2">
+                <MDBBtn
+                  color="secondary"
+                  type="button"
+                  onClick={() => clearCookieConsent()}
+                >
+                  {"Změnit volbu cookies"}
+                </MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
 
       <MDBTypography className="text-start">
         {"Pokud máte dotazy, kontaktujte nás na "}
